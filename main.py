097af76f1e687b0012464817e3efc9016a58d327
Valentin Achimescu <50949589+Achime11@users.noreply.github.com>
2023-09-30 19:47:22 +0300

    Implementing sync_folders function

--- main.py
+++ main.py
@@ -75,15 +75,50 @@ def logger_init():
 def synchronize_folders(source_folder, replica_folder):
     logger.info(f"Syncing \"{source_folder}\" to \"{replica_folder}\" !")
 
+    if not os.path.exists(source_folder):
+        logger.critical(f"Source folder '{source_folder}' does not exist!")
+        return
 
-def sync(scheduler, source_folder, replica_folder):
+    if not os.path.exists(replica_folder):
+        logger.info(f"Creating '{replica_folder}'!")
+        os.makedirs(replica_folder)
+
+    for item in os.listdir(source_folder):
+        # Creating full item path
+        source_item = os.path.join(source_folder, item)
+        replica_item = os.path.join(replica_folder, item)
+
+        # Checking if item is a folder to recall sync for folders
+        if os.path.isdir(source_item):
+            synchronize_folders(source_item, replica_item)
+        # else check for file in replica folder, if exist or time is different or size is different copy file
+        elif (not os.path.exists(replica_item) or
+              os.path.getmtime(source_item) != os.path.getmtime(replica_item) or
+              os.path.getsize(source_item) != os.path.getsize(replica_item) or
+              generate_file_md5(source_item) != generate_file_md5(replica_item)):
+            logger.info(f"Copying '{source_item}' to '{replica_item}'")
+            copy_file(source_item, replica_item)
+
+    # Remove files in replica folder that don't exist in source folder
+    for item in os.listdir(replica_folder):
+        replica_item = os.path.join(replica_folder, item)
+        source_item = os.path.join(source_folder, item)
+
+        if not os.path.exists(source_item) and os.path.isfile(replica_item):
+            logger.info(f"Removing '{replica_item}' (not in source folder)")
+            os.remove(replica_item)
+
+
+def sync(scheduler_param, source_folder, replica_folder):
     logger.info("Sync started!")
+
+    start_time = time.time()
 
     synchronize_folders(source_folder, replica_folder)
 
-    logger.info("Sync finished!")
+    logger.info(f"Sync finished after {time.time() - start_time} seconds!")
 
-    scheduler.enter(args.synchronization_interval, 1, sync, (scheduler, source_folder, replica_folder,))
+    scheduler_param.enter(args.synchronization_interval, 1, sync, (scheduler_param, source_folder, replica_folder,))
 
 
 # Create the parser
